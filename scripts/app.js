eae5fef37e06630361fa507aca95fb8f3f0468e9
summerdaysinbloom <bejustin7@gmail.com>
2015-01-23 16:44:29 +0800

    updated

--- scripts/app.js
+++ scripts/app.js
@@ -10,7 +10,7 @@ var app = angular.module('jamApp', [
 app.run(['$location', '$rootScope', '$timeout', '$routeParams', 
     function($location, $rootScope, $timeout, $routeParams, $templateCache) {
         $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
-            console.log('app')
+            console.log('app -')
             // $templateCache.put('templateId.html', 'This is the content of the template');
 
             // console.log($templateCache,'///');
@@ -25,9 +25,6 @@ app.run(['$location', '$rootScope', '$timeout', '$routeParams',
 }]);
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    
-    $locationProvider.html5Mode(true);
-
     $routeProvider
 	    .when('/', {
             templateUrl: 'views/main.html',
